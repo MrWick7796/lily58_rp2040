a0b155b8de20a63505f2471e3ba43425af816ac0
Harsh W <hwadha@inwerken.de>
2024-05-09 17:52:25 +0530

    Added comment about master slave

--- config.h
+++ config.h
@@ -1,30 +1,4 @@
-/*
-This is the c configuration file for the keymap
-
-Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 #pragma once
-
-/* Select hand configuration */
-
-// #define MASTER_LEFT
-// #define MASTER_RIGHT
-// #define EE_HANDS
 
 #define QUICK_TAP_TERM 0
 #define TAPPING_TERM 100
@@ -38,6 +12,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP17
 
+// Force master side determination by VBUS (USB voltage) detect method instead of the default USB data detect method. 
+// RP2040 controllers like Splinky and Sea Picro have working VBUS detect on GP19 pin. 
 #undef SPLIT_USB_DETECT
 #define USB_VBUS_PIN GP19
 
